dff8903967c56c05000c143d3b3e38ef74d7983d
aline dias necchi ribeiro <alinenecchi@gmail.com>
2021-06-01 22:39:31 -0300

    add laço para testes

--- bubble/bubblesort.py
+++ bubble/bubblesort.py
@@ -1,5 +1,13 @@
 # Vídeo "Bubble Sort": https://youtu.be/GiNPe_678Ms
+''' [8, 1, 4, 6, 9]
+        [1, 8, 4, 6, 9]
+            [1, 4, 8, 6, 9]
+                [1, 4, 6, 8, 9]
+                    [1, 4, 6, 8, 9]
+        '''
+
 import timeit
+
 def bubble_sort(lista):
     start_time = timeit.default_timer()
     n = len(lista)
@@ -9,6 +17,7 @@ def bubble_sort(lista):
                 # troca de elementos nas posições i e i+1
                 lista[i], lista[i+1] = lista[i+1], lista[i]
     final_time = timeit.default_timer() - start_time
-    print (final_time)
+
+    print(final_time)
 # Complexidade de tempo O(nˆ2)
 # Complexidade de espaço O(n)
